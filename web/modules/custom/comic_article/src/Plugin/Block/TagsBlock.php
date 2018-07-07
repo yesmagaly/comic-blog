6fb87be92ad460d81657cdcf487fdc05f08287dc
Yesica Ramirez <yesmagaly@gmail.com>
2018-07-07 18:49:06 -0500

    Refactored Tags block and block--tags template.

--- web/modules/custom/comic_article/src/Plugin/Block/TagsBlock.php
+++ web/modules/custom/comic_article/src/Plugin/Block/TagsBlock.php
@@ -3,10 +3,6 @@
 namespace Drupal\comic_article\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Entity\EntityManagerInterface;
-use Drupal\Core\Cache\Cache;
 
 /**
  * Provides a 'TagsBlock' block.
@@ -16,79 +12,58 @@ use Drupal\Core\Cache\Cache;
  *  admin_label = @Translation("Tags block"),
  * )
  */
-class TagsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class TagsBlock extends BlockBase {
 
-  /**
-   * Drupal\Core\Entity\EntityManagerInterface definition.
-   *
-   * @var \Drupal\Core\Entity\EntityManagerInterface
-   */
-  protected $entityManager;
-  /**
-   * Constructs a new TagsBlock object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param string $plugin_definition
-   *   The plugin implementation definition.
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    EntityManagerInterface $entity_manager
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->entityManager = $entity_manager;
-  }
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity.manager')
-    );
-  }
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    $build['#theme'] = 'tags__block';
+    $build['tags_block']['#markup'] = 'Implement TagsBlock.';
 
-    $path = \Drupal::service('path.current')->getPath();
-    $params = \Drupal\Core\Url::fromUserInput($path)->getRouteParameters();
+    $tids = \Drupal::entityQuery('taxonomy_term')
+      ->condition('vid', 'tags')
+      ->sort('changed', 'DESC')
+      ->execute();
 
-    if (isset($params['node'])) {
-      $node = \Drupal\node\Entity\Node::load($params['node']);
-      $tags = $node->get('field_tags');
+    $output = array_map(function ($tid) {
+      $amout = $this->getCountNodesByTag($tid);
+      $data = $this->getTagsMetadata($tid);
+      $data['amout'] = $amout;
 
-      # More info: https://www.flocondetoile.fr/blog/render-programmatically-unique-field-node-or-entity-drupal-8
-      $renderFieldTags = \Drupal::entityTypeManager()->getViewBuilder('node')
-        ->viewField($tags, 'full');
-
-      $build['#content'] = $renderFieldTags;
-
-      # Get caches.
-      $cacheTags = Cache::mergeTags(['comic:tags:block'], $node->getCacheTags());
-
-      # More info about cache context : https://www.drupal.org/docs/8/api/cache-api/cache-contexts
-      $cacheContexts = Cache::mergeTags(['url'], $node->getCacheContexts());
-
-      # Configure block caches.
-      # more info: https://weknowinc.com/blog/drupal-8-add-cache-metadata-render-arrays
-      $build['#cache'] = [
-        'tags' => $cacheTags,
-        'contexts' => $cacheContexts
-      ];
-    }
-
-    return $build;
+      return $data;
+    }, $tids);
+    
+    return [
+      '#theme' => 'tags__block',
+      '#tags' => $output,
+      '#cache' => [
+        'max-age' => 60,
+      ],
+    ];
   }
+
+  public function getCountNodesByTag ($tid) {
+    return \Drupal::entityQuery('node')
+      ->condition('field_tags', [$tid], 'IN')
+      ->condition('type', 'article')
+      ->count()
+      ->execute();
+  }
+
+  public function getTagsMetadata ($tid) {
+    $tag = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->load($tid);
+
+    $url = \Drupal::service('pathauto.generator')
+      ->createEntityAlias($tag, 'return');
+
+    return [
+      'name' => $tag->get('name')->value,
+      'url' => $url
+    ];
+  }
+
 
 }
